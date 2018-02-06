61d1c2852390a993239bddb5df2b14d0efba93b3
ericwallace904 <ewallace@wjxt.com>
2018-02-06 10:31:09 -0500

    toolbar controls
    added counties to toolbar control

--- js/intersection-crashes-script.js
+++ js/intersection-crashes-script.js
@@ -43,11 +43,127 @@ var navClay = L.Toolbar2.Action.extend({
 	}
 });
 
+var navStJohns = L.Toolbar2.Action.extend({
+	options: {
+		toolbarIcon: {
+			html: 'St. Johns County',
+			tooltip: 'St. Johns County'
+		}
+	},
+	
+	addHooks: function() {
+		map.setView([29.987,-81.482],11);
+	}
+});
+
+var navNassau = L.Toolbar2.Action.extend({
+	options: {
+		toolbarIcon: {
+			html: 'Nassau County',
+			tooltip: 'Nassau County'
+		}
+	},
+	
+	addHooks: function() {
+		map.setView([30.563,-81.801],11);
+	}
+});
+
+var navBaker = L.Toolbar2.Action.extend({
+	options: {
+		toolbarIcon: {
+			html: 'Baker County',
+			tooltip: 'Baker County'
+		}
+	},
+	
+	addHooks: function() {
+		map.setView([30.360,-82.394],11);
+	}
+});
+
+var navColumbia = L.Toolbar2.Action.extend({
+	options: {
+		toolbarIcon: {
+			html: 'Columbia County',
+			tooltip: 'Columbia County'
+		}
+	},
+	
+	addHooks: function() {
+		map.setView([30.211,-82.907],10);
+	}
+});
+
+var navUnion = L.Toolbar2.Action.extend({
+	options: {
+		toolbarIcon: {
+			html: 'Union County',
+			tooltip: 'Union County'
+		}
+	},
+	
+	addHooks: function() {
+		map.setView([30.033,-82.425],12);
+	}
+});
+
+var navBradford = L.Toolbar2.Action.extend({
+	options: {
+		toolbarIcon: {
+			html: 'Bradford County',
+			tooltip: 'Bradford County'
+		}
+	},
+	
+	addHooks: function() {
+		map.setView([29.930,-82.372],11);
+	}
+});
+
+var navPutnam = L.Toolbar2.Action.extend({
+	options: {
+		toolbarIcon: {
+			html: 'Putnam County',
+			tooltip: 'Putnam County'
+		}
+	},
+	
+	addHooks: function() {
+		map.setView([29.594,-81.912],11);
+	}
+});
+
+var navFlagler = L.Toolbar2.Action.extend({
+	options: {
+		toolbarIcon: {
+			html: 'Flagler County',
+			tooltip: 'Flagler County'
+		}
+	},
+	
+	addHooks: function() {
+		map.setView([29.466,-81.422],11);
+	}
+});
+
+var navAlachua = L.Toolbar2.Action.extend({
+	options: {
+		toolbarIcon: {
+			html: 'Alachua County',
+			tooltip: 'Alachua County'
+		}
+	},
+	
+	addHooks: function() {
+		map.setView([29.685,-82.479],11);
+	}
+});
 
 
 new L.Toolbar2.Control({
 	position: "topright",
-	actions: [navDuval,navClay]
+	actions: [navAlachua,navBaker,navBradford,navClay,navColumbia,navDuval,navFlagler,navNassau,navPutnam,navStJohns,navUnion]
 }).addTo(map)
 
 for (var num = 0; num < intersections.length; num++) {
